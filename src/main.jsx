66058e6552ad260c172bc91f25f88eabd73a5f18
Lucas <lucas-gabryel@outlook.com>
2024-07-01 18:57:15 -0300

    restart

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Home } from './pages/Home' // problema com o nome deails, letra maiuscula e minuscula
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    
+      <Home />
+
+  </React.StrictMode>
 )
