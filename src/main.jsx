9595ab47675ea72b0c9f526818d9f068253ca258
Lucas <lucas-gabryel@outlook.com>
2024-07-01 19:59:13 -0300

    create SignIn page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Home } from './pages/Home' // problema com o nome deails, letra maiuscula e minuscula
+import { SignIn } from './pages/SignIn' // problema com o nome deails, letra maiuscula e minuscula
 
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
