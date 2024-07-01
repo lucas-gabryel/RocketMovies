044d091cb7a01ccd7dd384f0a8a4df73492f6723
Lucas <lucas-gabryel@outlook.com>
2024-07-01 19:53:22 -0300

    add theme and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Home } from './pages/Home' // problema com o nome deails, letra maiuscula e minuscula
 
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+import { ThemeProvider } from 'styled-components';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Home />
-
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+    COLORS: {
+        BACKGROUND_PRIMARY: "#1C1B1E",
+        BACKGROUND_SECONDARY: "#262529",
+
+        PARAGRAPH: "#948F99",
+        
+        PINK: "#FF859B",
+        WHITE: "#FFFFFF",
+        BLACK: "#000000",
+
+    }
+}
